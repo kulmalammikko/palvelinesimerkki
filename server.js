f0dceb7206c491e423c1b21a41c236e4e7b825f2
kulmalammikko <mikko.kulmala@seamk.fi>
2022-09-14 10:24:18 +0300

    Serverin perusteiden luonti

---
+++ server.js
@@ -0,0 +1,21 @@
+// Ota express käyttöön
+const express =  require('express');
+const app = express();
+
+// Ota mongoose käyttöön -> tietokantayhteys
+const mongoose = require('mongoose');
+
+//Ota mongodb käyttöön -- palataan asiaan, tarviiko asentaa erikseen
+const mongodb = require('mongodb');
+
+//Ota bodyparser käyttöön lomakkeen käsittelyä varten
+const bodyparser = require('body-parser');
+
+//Aseta määritykset express-palvelimelle
+//Ota käyttöön public-tiedosto
+app.use(express.static('public'));
+    //Ota käyttöön bodyparser
+ app.use(bodyparser.urlencoded({extended:false}));
+
+//Laitetaan palvelin kuuntelemaan porttia 8080
+const server = app.listen(8080, function(){})
