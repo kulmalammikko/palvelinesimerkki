c4490f4f98bfa51023fd318733d0785b57cd2535
kulmalammikko <mikko.kulmala@seamk.fi>
2022-09-19 08:42:07 +0300

    kantaskeema

---
+++ bookSchema.js
@@ -0,0 +1,25 @@
+//Otetaan Mongoose käytöön
+const mongoose = require('mongoose');
+
+const bookSchema = mongoose.Schema;
+
+//Muokkaa tämä osion harjoitustyössä oman tietorakenteen mukaan
+let book = new bookSchema ( {
+    title: {
+        type: String
+    },
+    author: {
+        type: String
+    },
+    publisher: {
+        type: String
+    },
+    read: {
+        type: boolean
+    }
+},
+    { collection: 'books'}
+);
+
+// Export model, huom! Parametreina kokoelman nimi ja skeeman nimi, jotka pitää vaihtaa harkkatyön mukaiseksi.
+module.exports = mongoose.model('books', book);
